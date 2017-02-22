645bad20500f80a9af5e05125481cd57eca1593d
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-02-22 09:08:37 +0100

    Parameterize BootStrappedParser test

--- src/test/java/nl/dvberkel/peg/bootstrap/BootStrappedParserTest.java
+++ src/test/java/nl/dvberkel/peg/bootstrap/BootStrappedParserTest.java
@@ -3,8 +3,12 @@ package nl.dvberkel.peg.bootstrap;
 import nl.dvberkel.peg.Ast;
 import nl.dvberkel.peg.Parser;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static nl.dvberkel.peg.bootstrap.BootStrappedParser.characterClass;
 import static nl.dvberkel.peg.bootstrap.BootStrappedParser.definition;
@@ -12,14 +16,30 @@ import static nl.dvberkel.peg.bootstrap.BootStrappedParser.grammar;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+@RunWith(Parameterized.class)
 public class BootStrappedParserTest {
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        Collection<Object[]> data = new ArrayList<>();
+        data.add(new Object[]{"test/resources/grammars/simple.peg", grammar(definition("a", characterClass()))});
+        return data;
+    }
+
+    private final String grammarLocation;
+    private final Ast expectedAst;
+
+    public BootStrappedParserTest(String grammarLocation, Ast expectedAst) {
+        this.grammarLocation = grammarLocation;
+        this.expectedAst = expectedAst;
+    }
+
     @Test
     public void shouldParseSimpleGrammar() {
         Parser parser = new BootStrappedParser();
 
-        Ast ast = parser.parse(Paths.get("test/resources/grammars/simple.peg"));
+        Ast ast = parser.parse(Paths.get(grammarLocation));
 
-        assertThat(ast, is(grammar(definition("a", characterClass()))));
+        assertThat(ast, is(expectedAst));
 
     }
 }
